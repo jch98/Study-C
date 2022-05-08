93672252378b82faaebdf98f7b75bdfa7a932e33
jch98 <ehdrychemdgk@gmail.com>
2022-05-08 23:47:49 +0900

    Create main.c

---
+++ chap03/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+void main () {
+
+	int a;
+	a = 5;
+	char c = 'A';
+	printf("%d \n", a);
+	printf("%c %d\n", c, c);
+
+	system("pause");
+
+
+
+	/*
+	char fruit[20] = "strawberry";
+	char*  p;
+	printf("%s\n", fruit);
+	p = "banana";
+	printf("%s\n", p);
+	system("pause");*/
+	
+}
+
+
+
+
+
+
